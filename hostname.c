356cff634be25347d843bf918753f814aaaf6233
Madhav Chauhan <madhav.chauhan@intel.com>
2017-12-19 17:54:44 +0530

    first commit

---
+++ hostname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+        char hostname[100];
+            gethostname(hostname, 100);
+
+                //puts(hostname);
+                printf("HOSTNAME:%s\n",hostname);
+
+                    return EXIT_SUCCESS;
+}
