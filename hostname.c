651af0f552562d11d25d2b17f81c4e4697443aae
Madhav Chauhan <madhav.chauhan@intel.com>
2018-02-05 13:30:48 +0530

    remove redundant code

--- hostname.c
+++ hostname.c
@@ -11,7 +11,6 @@ int main(void) {
         char hostname[100];
             gethostname(hostname, 100);
 
-                //puts(hostname);
                 printf("HOSTNAME:%s\n",hostname);
 
                     return EXIT_SUCCESS;
